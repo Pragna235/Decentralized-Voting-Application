561c5287f71f31fa0b99410bda98c59d07397c6e
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2024-02-28 05:40:23 +0530

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,7 +2,7 @@ async function main() {
   const Voting = await ethers.getContractFactory("Voting");
 
   // Start deployment, returning a promise that resolves to a contract object
-  const Voting_ = await Voting.deploy(["Jane Rizzoli", "Maura Isles", "Angela", "Korsak","Frank Rizzoli"], 180);
+  const Voting_ = await Voting.deploy(["Jane Rizzoli", "Maura Isles", "Angela", "Korsak","Frank Rizzoli"], 525960);
   console.log("Contract address:", Voting_.address);
 
 
